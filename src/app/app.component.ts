a18bd54b32d276c640bfcaecfcfc5c881bb9cd78
mrblack360 <rmaswi360@outlook.com>
2020-04-30 02:17:49 +0300

    feat(app): added the buttons at the toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'COVID-19 dashboard';
+  title = 'Dashboard';
+  buttons = [
+    { label: 'Habari', link: '' },
+    { label: 'Kuhusu', link: '' },
+    { label: 'Msaada', link: '' },
+  ];
   footer = 'Maswi(mrblack360) • Dashboard • (stable) ∧ @0.0.0';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
