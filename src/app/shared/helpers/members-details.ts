e69c8d398735ccd9ee10f5bee09ceb069ab0cc38
mrblack360 <rmaswi360@outlook.com>
2020-05-07 00:24:59 +0300

    feat(helpers): added some members details

---
+++ src/app/shared/helpers/members-details.ts
@@ -0,0 +1,53 @@
+export const Members = [
+  {
+    group: 'bantu',
+    members: [
+      {
+        name: 'Chingalo, Eric',
+        nickName: 'Megamind',
+        imageUrl: 'chingalo.png',
+        links: [
+          'echingalo@gmail.com',
+          '255683397035',
+          'eric.chingalo',
+          'ericchingalo',
+          'eric-chingalo-711630185/',
+        ],
+      },
+    ],
+  },
+  {
+    group: 'Trench',
+    members: [
+      {
+        name: 'Chingalo, Eric',
+        nickName: 'Megamind',
+        imageUrl: 'chingalo.png',
+        links: [
+          'echingalo@gmail.com',
+          '255683397035',
+          'eric.chingalo',
+          'ericchingalo',
+          'eric-chingalo-711630185/',
+        ],
+      },
+    ],
+  },
+  {
+    group: 'Wanaa',
+    members: [
+      {
+        name: 'Chingalo, Eric',
+        nickName: 'Megamind',
+        imageUrl: 'chingalo.png',
+        links: [
+          'echingalo@gmail.com',
+          '255683397035',
+          'eric.chingalo',
+          'ericchingalo',
+          'eric-chingalo-711630185/',
+        ],
+      },
+    ],
+  },
+];
