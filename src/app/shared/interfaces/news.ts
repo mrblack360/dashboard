7bb989d1017a7ad32c2e69bcb9bf1a49cc80a4bc
mrblack360 <rmaswi360@outlook.com>
2020-05-08 15:36:51 +0300

    feat(shared): created the news interface

---
+++ src/app/shared/interfaces/news.ts
@@ -0,0 +1,17 @@
+export interface News {
+  timestamp: number;
+  articleCount: number;
+  articles: [
+    {
+      title: string;
+      description: string;
+      url: string;
+      image: string;
+      publishedAt: string;
+      source: {
+        name: string;
+        url: string;
+      };
+    }
+  ];
+}
