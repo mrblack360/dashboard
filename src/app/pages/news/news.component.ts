2abac50b0011b2f570f9d94cd611ba76cc016dae
mrblack360 <rmaswi360@outlook.com>
2020-05-08 15:53:34 +0300

    feat(news): created the news views

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NewsService } from '../../shared/services/news/news.service';
+import { News } from '../../shared/interfaces/news';
+
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.css'],
 })
 export class NewsComponent implements OnInit {
-  constructor() {}
+  news: News;
+  date = new Date();
+  constructor(private newsService: NewsService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.newsService.getNews().subscribe((news: News) => {
+      this.news = news;
+    });
+  }
 }
