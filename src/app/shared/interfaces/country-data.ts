0bb7c9ebc26ba966527ea42a1c0f22d106f27dd9
mrblack360 <rmaswi360@outlook.com>
2020-05-13 15:46:35 +0300

    refactor(interfaces): created the country-data interface

---
+++ src/app/shared/interfaces/country-data.ts
@@ -0,0 +1,14 @@
+export interface CountryData {
+  Active?: number;
+  City?: string;
+  CityCode?: string;
+  Confirmed?: number;
+  Country?: string;
+  CountryCode?: string;
+  Date?: string;
+  Deaths?: number;
+  Lat?: string;
+  Lon?: string;
+  Province?: string;
+  Recovered?: number;
+}
